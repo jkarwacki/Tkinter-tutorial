23e40a68cf51c1b1cf9b395864c8735adf4496a6
Julian <karwacki@kth.se>
2020-05-07 00:06:00 +0200

    up to part 12

--- OOP_tutorial.py
+++ OOP_tutorial.py
@@ -10,12 +10,34 @@ from matplotlib import style
 import tkinter as tk
 from tkinter import ttk
 
+import pandas as pd
+import numpy as np
+
+import urllib
+import json
+
+import pandas as pd
+import numpy as np
+
+from matplotlib import pyplot as plt
+
 LARGE_FONT = ("Verdana", 12)
+NORMAL_FONT = ("Verdana", 10)
+SMALLL_FONT = ("Verdana", 8)
 style.use("ggplot")
 
-f = Figure(figsize=(5, 5), dpi=100)
+f = Figure()
 a = f.add_subplot(111)
 
+
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORMAL_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Ok", command=popup.destroy)
+    B1.pack()
+    popup.mainloop()
 
 def animate(i):
     pullData = open("sampleData.txt", "r").read()
@@ -30,6 +52,7 @@ def animate(i):
 
     a.clear()
     a.plot(xList, yList)
+
 
 class SeaofBTCapp(tk.Tk):
 
@@ -47,9 +70,18 @@ class SeaofBTCapp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        menubar = tk.Menu(container)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Save Settings", command = lambda: popupmsg("Not supported yet"))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        tk.Tk.config(self, menu=menubar)
+
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo, PageThree):
+        for F in (StartPage, PageOne, BTCe_page):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -69,20 +101,19 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label = tk.Label(self, text="""ALPHA Bitcoin trading application
+        use at your own risk.
+        this is only beta""", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Visit Page 1",
-                             command=lambda: controller.show_frame(PageOne))  # qf("This worked"))
+        button1 = ttk.Button(self, text="Agree",
+                             command=lambda: controller.show_frame(BTCe_page))  # qf("This worked"))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Visit Page 2",
-                             command=lambda: controller.show_frame(PageTwo))  # qf("This worked"))
+        button2 = ttk.Button(self, text="Disagree",
+                             command=quit)  # qf("This worked"))
         button2.pack()
 
-        button3 = ttk.Button(self, text="Graph Page",
-                             command=lambda: controller.show_frame(PageThree))  # qf("This worked"))
-        button3.pack()
 
 
 class PageOne(tk.Frame):
@@ -96,28 +127,7 @@ class PageOne(tk.Frame):
                              command=lambda: controller.show_frame(StartPage))  # qf("This worked"))
         button1.pack()
 
-        button1 = ttk.Button(self, text="Page Two",
-                             command=lambda: controller.show_frame(PageTwo))  # qf("This worked"))
-        button1.pack()
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Two", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))  # qf("This worked"))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page One",
-                             command=lambda: controller.show_frame(PageOne))  # qf("This worked"))
-        button2.pack()
-
-
-class PageThree(tk.Frame):
+class BTCe_page(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -137,5 +147,10 @@ class PageThree(tk.Frame):
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand = True)
 
 app = SeaofBTCapp()
+
+positionRight = int(app.winfo_screenwidth()/2 - 800/2)
+positionDown = int(app.winfo_screenheight()/3 - 600/3)
+app.geometry("800x600+{}+{}" .format(positionRight, positionDown))
+
 ani = animation.FuncAnimation(f, animate, interval=1000)
 app.mainloop()
